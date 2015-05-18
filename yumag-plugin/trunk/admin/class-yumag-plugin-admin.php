e017ec7c26c3747ccab08feb72f38d42d30f7c49
chris barnes <mail@cjbarnes.co.uk>
2015-05-18 12:03:55 +0100

    Remove unused styles and scripts

--- yumag-plugin/trunk/admin/class-yumag-plugin-admin.php
+++ yumag-plugin/trunk/admin/class-yumag-plugin-admin.php
@@ -49,66 +49,8 @@ class YuMag_Plugin_Admin extends YuMag_Plugin_Singleton {
 	 */
 	protected function define_hooks() {
 
-		// Enqueue public-facing styles and scripts.
-		// add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
-		// add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_styles' ) );
-
 		// Filter the hidden metaboxes on the Edit Post page.
 		add_filter( 'default_hidden_meta_boxes', array( $this, 'show_excerpt_by_default' ), 10, 2 );
-
-	}
-
-	/**
-	 * Register the stylesheets for the Dashboard.
-	 *
-	 * @since 1.0.0
-	 */
-	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be hooked to the
-		 * `wp_enqueue_scripts` action by this class's `define_hooks()` method,
-		 * which is called by the parent class's constructor.
-		 */
-
-		$plugin = YuMag_Plugin::get_instance();
-
-		wp_enqueue_style(
-			$plugin->get_plugin_name(),
-			plugin_dir_url( __FILE__ ) . 'css/yumag-plugin-admin.css',
-			array(),
-			$plugin->get_version(),
-			'all'
-		);
-
-	}
-
-	/**
-	 * Register the JavaScript for the dashboard.
-	 *
-	 * @since 1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be hooked to the
-		 * `wp_enqueue_scripts` action by this class's `define_hooks()` method,
-		 * which is called by the parent class's constructor.
-		 */
-
-		$plugin = YuMag_Plugin::get_instance();
-
-		wp_enqueue_script(
-			$plugin->get_plugin_name(),
-			plugin_dir_url( __FILE__ ) . 'js/yumag-plugin-admin.js',
-			array( 'jquery' ),
-			$plugin->get_version(),
-			false
-		);
 
 	}
 
--- yumag-plugin/trunk/public/class-yumag-plugin-public.php
+++ yumag-plugin/trunk/public/class-yumag-plugin-public.php
@@ -31,10 +31,6 @@ class YuMag_Plugin_Public extends YuMag_Plugin_Singleton {
 	 */
 	protected function define_hooks() {
 
-		// Enqueue public-facing styles and scripts.
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
-
 		/*
 		 * Action to attach more actions/filters to other plugins' hooks.
 		 * Called on plugins loaded so that I can get the instances of classy
@@ -62,60 +58,6 @@ class YuMag_Plugin_Public extends YuMag_Plugin_Singleton {
 			add_filter( 'wps_subtitle', array( $widont, 'widont' ), 100 );
 
 		}
-
-	}
-
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since 1.0.0
-	 */
-	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be hooked to the
-		 * `wp_enqueue_scripts` action by this class's `define_hooks()` method,
-		 * which is called by the parent class's constructor.
-		 */
-
-		$plugin = YuMag_Plugin::get_instance();
-
-		wp_enqueue_style(
-			$plugin->get_plugin_name(),
-			plugin_dir_url( __FILE__ ) . 'css/yumag-plugin-public.css',
-			array(),
-			$plugin->get_version(),
-			'all'
-		);
-
-	}
-
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since 1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be hooked to the
-		 * `wp_enqueue_scripts` action by this class's `define_hooks()` method,
-		 * which is called by the parent class's constructor.
-		 */
-
-		$plugin = YuMag_Plugin::get_instance();
-
-		wp_enqueue_script(
-			$plugin->get_plugin_name(),
-			plugin_dir_url( __FILE__ ) . 'js/yumag-plugin-public.js',
-			array( 'jquery' ),
-			$plugin->get_version(),
-			false
-		);
 
 	}
 
